621434b9b8907686cacd87ddd78e09e4695b625f
ibukunolatunde1 <ibukunolatunde1@gmail.com>
2019-09-12 20:44:13 +0100

    Views for all routes

--- app.js
+++ app.js
@@ -11,8 +11,12 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const ownerRoutes = require('./routes/owner');
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', ownerRoutes);
 
 mongoose
     .connect(process.env.DB_URL)
